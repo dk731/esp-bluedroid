f21bacfe18b8ab74c6c41ac20d0f5d770df736e7
Demid Kaidalov <demid.kaidalov@gmail.com>
2025-02-03 17:06:04 +0200

    Enhance BLE GATT server example and update configurations
    - Updated runner command in `.cargo/config.toml` to include partition table.
    - Added new dependencies `heapless` and `enumset` in `Cargo.toml`.
    - Created new partition configuration file `partitions/esp32-ota-8m.csv`.
    - Expanded Bluetooth configuration options in `sdkconfig.defaults`.
    - Implemented a BLE GATT server example in `src/lib.rs`.
    - Modified `src/main.rs` to call the example server and handle errors.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
+use esp_bluedroid::example;
+
 fn main() {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
-    esp_idf_svc::sys::link_patches();
+    // esp_idf_svc::sys::link_patches();
 
-    // Bind the log crate to the ESP Logging facilities
-    esp_idf_svc::log::EspLogger::initialize_default();
+    // // Bind the log crate to the ESP Logging facilities
+    // esp_idf_svc::log::EspLogger::initialize_default();
 
     log::info!("Hello, world! 123");
 
-    let mut i = 0;
-    loop {
-        i += 1;
-        log::info!("Hello, world! {}", i);
-        std::thread::sleep(std::time::Duration::from_secs(1));
+    let result = example::main();
+    if let Err(e) = result {
+        log::error!("Error: {:?}", e);
     }
+
+    // let mut i = 0;
+    // loop {
+    //     i += 1;
+    //     log::info!("Hello, world! {}", i);
+    //     std::thread::sleep(std::time::Duration::from_secs(1));
+    // }
 }
