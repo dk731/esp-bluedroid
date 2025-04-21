522c7ed741432cab511905aa9dba517c34292f79
Demid Kaidalov <demid.kaidalov@gmail.com>
2025-04-21 13:50:12 +0300

    Resolve merge conflicts in configuration files and clean up code: update .cargo/config.toml, .gitignore, launch.json, Cargo.toml, esp32-ota-8m.csv, sdkconfig.defaults, lib.rs, main.rs to remove conflict markers and ensure proper formatting.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 use esp_bluedroid::{
     ble,
     gatts::{
@@ -89,29 +88,3 @@ fn run_ble_example() -> anyhow::Result<()> {
 
     Ok(())
 }
-=======
-use esp_bluedroid::example;
-
-fn main() {
-    // It is necessary to call this function once. Otherwise some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
-    // esp_idf_svc::sys::link_patches();
-
-    // // Bind the log crate to the ESP Logging facilities
-    // esp_idf_svc::log::EspLogger::initialize_default();
-
-    log::info!("Hello, world! 123");
-
-    let result = example::main();
-    if let Err(e) = result {
-        log::error!("Error: {:?}", e);
-    }
-
-    // let mut i = 0;
-    // loop {
-    //     i += 1;
-    //     log::info!("Hello, world! {}", i);
-    //     std::thread::sleep(std::time::Duration::from_secs(1));
-    // }
-}
->>>>>>> 3f16bc07cb88de0e0153b806fb79d81afe2aa196
