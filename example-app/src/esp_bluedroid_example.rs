0d3cae36c3c4700eef6c3fa71139edd3dbf6cbe0
Demid Kaidalov <demid.kaidalov@gmail.com>
2025-05-08 17:55:44 +0300

    Update dependencies and add example applications for BLE GATT server; include esp_bluedroid_example and esp_idf_example

---
+++ example-app/src/esp_bluedroid_example.rs
@@ -0,0 +1,136 @@
+use std::sync::Arc;
+
+use esp_bluedroid::{
+    ble,
+    gap::GapConfig,
+    gatts::{
+        app::App,
+        attribute::{
+            AttributeUpdate,
+            defaults::{U16Attr, U32Attr},
+        },
+        characteristic::{Characteristic, CharacteristicConfig},
+        descriptor::{Descriptor, DescriptorConfig},
+        service::Service,
+    },
+    svc::{
+        bt::{
+            BtUuid,
+            ble::gatt::{GattId, GattServiceId},
+        },
+        hal::prelude::Peripherals,
+    },
+};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct FooBar {
+    bar: String,
+    foo_bar: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct CoolNestedChar {
+    bar: String,
+    foo_bar: FooBar,
+
+    temperature: u16,
+    messages: Vec<String>,
+}
+
+pub fn main() -> anyhow::Result<()> {
+    esp_bluedroid::svc::sys::link_patches();
+    esp_bluedroid::svc::log::EspLogger::initialize_default();
+
+    run_ble_example()?;
+
+    Ok(())
+}
+
+fn run_ble_example() -> anyhow::Result<()> {
+    let peripherals = Peripherals::take()?;
+    let ble = ble::Ble::new(peripherals.modem)?;
+    let app = ble.gatts.register_app(&App::new(0))?;
+
+    let service = app.register_service(&Service::new(
+        GattServiceId {
+            id: GattId {
+                uuid: BtUuid::uuid128(1),
+                inst_id: 0,
+            },
+            is_primary: true,
+        },
+        20,
+    ))?;
+
+    let char1 = service.register_characteristic(&Characteristic::new(
+        U16Attr(0),
+        CharacteristicConfig {
+            uuid: BtUuid::uuid128(2),
+            value_max_len: 100,
+            readable: true,
+            writable: true,
+            broadcasted: true,
+            enable_notify: true,
+            description: Some("Test characteristic".to_string()),
+        },
+        Some(vec![Arc::new(Descriptor::new(
+            U32Attr(777),
+            DescriptorConfig {
+                uuid: BtUuid::uuid128(123),
+                readable: true,
+                writable: true,
+            },
+        ))]),
+    ))?;
+
+    let _char2 = service.register_characteristic(&Characteristic::new(
+        CoolNestedChar {
+            bar: "Hello".to_string(),
+            foo_bar: FooBar {
+                bar: "World".to_string(),
+                foo_bar: "FooBar".to_string(),
+            },
+            temperature: 0,
+            messages: vec!["Hello".to_string(), "World".to_string()],
+        },
+        CharacteristicConfig {
+            uuid: BtUuid::uuid128(3),
+            value_max_len: 100,
+            readable: true,
+            writable: true,
+            broadcasted: true,
+            enable_notify: true,
+            description: Some("Test characteristic".to_string()),
+        },
+        None,
+    ))?;
+
+    let thread_char = char1.clone();
+    std::thread::spawn(move || {
+        for AttributeUpdate { old, new } in thread_char.0.attribute.updates_rx.iter() {
+            log::info!("Characteristic was update.\tOld: {:?}\tNew: {:?}", old, new);
+        }
+    });
+
+    service.start()?;
+    ble.gap.set_config(GapConfig {
+        device_name: "esp-bluedroid".to_string(),
+        max_connections: Some(3),
+        manufacturer_data: Some("ESP-IDF".as_bytes().to_vec()),
+        ..GapConfig::default()
+    })?;
+    ble.gap.start_advertising()?;
+
+    let mut i = 0;
+    loop {
+        std::thread::sleep(std::time::Duration::from_secs(5));
+
+        if let Err(err) = char1.update_value(U16Attr(i)) {
+            log::error!("Failed to update value: {:?}", err);
+        }
+        i += 1;
+    }
+
+    Ok(())
+}
---
+++ example-app/src/lib.rs
@@ -0,0 +1,2 @@
+pub mod esp_bluedroid_example;
+pub mod esp_idf_example;
